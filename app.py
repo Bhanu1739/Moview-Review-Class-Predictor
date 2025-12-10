1b3c8c806e59f3d16e225d9c314e71166c2f6ba5
Bhanu <cpbhanuprakashreddy@gmail.com>
2025-12-10 22:25:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+import sklearn
+import pandas as pd
+import numpy as np
+
+with open("nb_model.pkl","rb") as f:
+    model = pickle.load(f)
+
+st.title("Movie Review Positive/Negative Class Predictor")
+st.write("Bhanu Prakash Reddy ChinnaPashula")
+
+review = st.text_input("review")
+
+if st.button("Predict Review"):
+    input_df = pd.DataFrame([
+        "review" : review
+    ])
+
+    result = model.predict(input_df)[0]
+    st.success(f"Predicted Review Class : {result}")
